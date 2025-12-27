9397f51cea84632e20128b14229d99031dfd389c
nabeel <mohammadnabeeljameel@gmail.com>
2025-12-27 12:46:54 +0500

    Remove unnecessary top border divider from HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function HomePage() {
   return (
     <main className="min-h-screen">
       <Navbar />
-      <div className="mt-2.5 border-t border-dotted border-border"></div>
       <HeroSection />
       <div className="mt-2.5 border-b border-dotted border-border"></div>
       <IndustryPartners />
